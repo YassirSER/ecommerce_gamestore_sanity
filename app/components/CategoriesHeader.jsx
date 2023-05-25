df74279e4e0c03c53ecbe5544e2ce7655b12d326
RandomGuy <yassirmaroc2006@hotmail.com>
2023-05-25 01:57:05 +0100

    sorting works :)

--- app/components/CategoriesHeader.jsx
+++ app/components/CategoriesHeader.jsx
@@ -1,19 +1,44 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const CategoriesHeader = () => {
+  const pathname = usePathname();
+
   return (
     <div className="categories-container">
       <div className="category">
-        <a href="/products/xboxgame">Xbox games</a>
+        <Link
+          href="/products/xboxgame"
+          className={pathname.startsWith("/products/xboxgame") ? "active" : ""}
+        >
+          Xbox games
+        </Link>
       </div>
       <div className="category">
-        <a href="/products/steamgame">Steam games</a>
+        <Link
+          href="/products/steamgame"
+          className={pathname.startsWith("/products/steamgame") ? "active" : ""}
+        >
+          Steam games
+        </Link>
       </div>
       <div className="category">
-        <a href="/products/psgame"> PS games </a>
+        <Link
+          href="/products/psgame"
+          className={pathname.startsWith("/products/psgame") ? "active" : ""}
+        >
+          {" "}
+          PS games{" "}
+        </Link>
       </div>
       <div className="category">
-        <a href="/products/software">Software</a>
+        <Link
+          href="/products/software"
+          className={pathname.startsWith("/products/software") ? "active" : ""}
+        >
+          Software
+        </Link>
       </div>
     </div>
   );
--- app/products/[category]/CategorisedProducts.jsx
+++ app/products/[category]/CategorisedProducts.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 
 import { Product, Dropdown } from "../../components/index";
 
@@ -8,25 +8,40 @@ const CategorisedProducts = ({ products }) => {
   const options = [
     { value: "ASC", label: "ASC" },
     { value: "DESC", label: "DESC" },
-    // { value: "red", label: "Red" },
   ];
+
+  const [filter, setFilter] = useState("");
+
+  const sortProducts = (sortType) => {
+    if (sortType === "ASC") {
+      return products.sort((a, b) => (a["price"] < b["price"] ? -1 : 1));
+    } else if (sortType === "DESC") {
+      return products.sort((a, b) => (b["price"] > a["price"] ? 1 : -1));
+    }
+  };
 
   return (
     <>
       <div className="sorting-container">
         <Dropdown
-          isSearchable
+          //   isSearchable
           //   isMulti
           placeHolder="Sort by"
           options={options}
-          //   onChange={(value) => console.log(value)}
+          onChange={(value) => {
+            setFilter(value.value);
+          }}
         />
       </div>
 
       <div className="products-container">
-        {products?.map((product) => (
-          <Product key={product._id} product={product} />
-        ))}
+        {filter.length > 1
+          ? sortProducts(filter).map((product) => (
+              <Product key={product._id} product={product} />
+            ))
+          : products?.map((product) => (
+              <Product key={product._id} product={product} />
+            ))}
       </div>
     </>
   );
