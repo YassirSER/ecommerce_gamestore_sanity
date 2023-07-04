9d04c213ee1ef669b70c59cc92f12e7361a2da9a
RandomGuy <yassirmaroc2006@hotmail.com>
2023-07-04 17:29:40 +0100

    fixing some errors

--- app/search/Loading.jsx
+++
@@ -1,6 +0,0 @@
-import { FaSpinner } from "react-icons/fa";
-
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <FaSpinner className="loaderIcon" />;
-}
