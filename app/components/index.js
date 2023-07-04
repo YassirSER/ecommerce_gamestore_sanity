2dfbe38c2391feb00684478e286a5cfa82f1da6b
RandomGuy <yassirmaroc2006@hotmail.com>
2023-07-04 17:34:19 +0100

    same fixing

--- app/components/index.js
+++ app/components/index.js
@@ -8,8 +8,6 @@ export { default as Layout } from "./Layout";
 export { default as ProductDetailsClient } from "./ProductDetailsClient";
 export { default as Dropdown } from "./Dropdown";
 export { default as ProductsWithSorting } from "./ProductsWithSorting";
-// export { default as SearchBar } from "./SearchBar";
-export { default as Loading } from "../search/Loading";
 export { default as SearchBar } from "./SearchBar";
 export { default as SearchHits } from "./SearchHits";
 export { default as Search } from "./Search";
