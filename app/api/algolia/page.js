3c4ade4055cdc23cf54f4d16eb80855d0b9a3561
RandomGuy <yassirmaroc2006@hotmail.com>
2023-07-13 03:03:30 +0100

    algolia plz work

--- app/api/algolia/page.js
+++ app/api/algolia/page.js
@@ -1,8 +1,8 @@
 import algoliasearch from "algoliasearch";
 import sanityClient from "@sanity/client";
 import indexer from "sanity-algolia";
-import { client } from "../../lib/client";
-import { algolia } from "@/lib/algolia";
+import { client } from "../../../lib/client";
+import { algolia } from "../../../lib/algolia";
 
 export default function handler(req, res) {
   const sanityAlgolia = indexer(
@@ -12,16 +12,16 @@ export default function handler(req, res) {
       },
     },
     (document) => {
-      switch (document._type) {
+      switch (document?._type) {
         case "product":
           return {
-            name: document.name,
-            slug: document.slug.current,
-            price: document.price,
-            image: document.image,
+            name: document?.name,
+            slug: document?.slug.current,
+            price: document?.price,
+            image: document?.image,
           };
         default:
-          throw new Error(`Unknown type: ${document.type}`);
+          throw new Error(`Unknown type: ${document?._type}`);
       }
     }
   );
